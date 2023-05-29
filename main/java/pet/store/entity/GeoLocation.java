ea298955d21f1fc8f75fb9d7516a213567ee68e0
Anna Yi <127422303+BE-AKY@users.noreply.github.com>
2023-05-29 19:32:44 -0400

    Intro to Spring Boot

---
+++ main/java/pet/store/entity/GeoLocation.java
@@ -0,0 +1,24 @@
+package pet.store.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+public class GeoLocation {
+
+	private BigDecimal latitude;
+	private BigDecimal longitude;
+	
+	public GeoLocation(GeoLocation geoLocation) {
+		this.latitude = geoLocation.latitude;
+		this.longitude = geoLocation.longitude;
+	}
+	
+	
+} //End of Class
+ 
